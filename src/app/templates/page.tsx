454319713c4c53c2c4f4619e2d0c773f55fab244
Salvador Camino <salvadorcamino@gmail.com>
2024-11-15 22:18:55 -0300

    ip templates

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -52,10 +52,6 @@ const templatesIP = () => {
 
     <h1 className="text-4xl font-bold text-center mb-8">IP Templates</h1>
   
-      
-      
-      
-        
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
