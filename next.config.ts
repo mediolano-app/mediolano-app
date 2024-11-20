82210e1fd6bdd0543287380982efe80fe10c92b7
Salvador Camino <salvadorcamino@gmail.com>
2024-11-20 20:16:13 -0300

    rebase

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  
-  /* config options here */
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
-
-
 export default nextConfig;
-
