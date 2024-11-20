ec17e743d02c5747968a01f4e5a7fed848651a9b
Salvador Camino <salvadorcamino@gmail.com>
2024-11-20 20:12:44 -0300

    build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  
   /* config options here */
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -9,4 +18,7 @@ const nextConfig: NextConfig = {
   },
 };
 
+
+
 export default nextConfig;
+
