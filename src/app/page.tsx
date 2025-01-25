25c5aa03406cb96fceecffb35cafd3a288f33aeb
Salvador Camino <salvadorcamino@gmail.com>
2025-01-25 09:30:38 -0300

    startpage updt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ import dynamic from 'next/dynamic';
 import { useAccount, useBalance, useBlockNumber, useContract, useReadContract, useSendTransaction, useTransactionReceipt } from '@starknet-react/core';
 import { BlockNumber, Contract, RpcProvider } from "starknet";
 import { formatAmount, shortenAddress } from '@/lib/utils';
-import StartPage2 from "@/components/StartPage2";
+import StartPage from "@/components/StartPage";
 
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 gap-16 sm:p-16">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <StartPage2 />
+        <StartPage />
       </main>
     </div>
   );
