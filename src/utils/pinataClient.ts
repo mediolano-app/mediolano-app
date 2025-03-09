79b08b802344d6ec04bfd9991c19a50d27b81441
PedroRosalba <pedro.dandrea.rpd@gmail.com>
2025-03-09 01:08:44 -0300

    continue small fixes

--- src/utils/pinataClient.ts
+++ src/utils/pinataClient.ts
@@ -1,11 +1,6 @@
 import { PinataSDK } from "pinata-web3";
 
 export const pinataClient = new PinataSDK({
-<<<<<<< Updated upstream
-	pinataJwt: `${process.env.PINATA_JWT}`,
-	pinataGateway: 'lavender-quickest-reptile-91.mypinata.cloud',
-=======
   pinataJwt: process.env.PINATA_JWT || "",
   pinataGateway: process.env.PINATA_HOST || "violet-adjacent-skink-713.mypinata.cloud" 
->>>>>>> Stashed changes
 });
