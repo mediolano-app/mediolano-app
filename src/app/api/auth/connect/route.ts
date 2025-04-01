06e0fdd616d897fd2329b428a404684da20f2c51
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2025-04-01 06:17:23 +0100

    made the verification work

--- src/app/api/auth/connect/route.ts
+++ src/app/api/auth/connect/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url)
     const address = searchParams.get('address');
-    console.log("GOod here")
+    // console.log("GOod here")
 
     if (!address) {
         return NextResponse.json(
@@ -15,26 +15,26 @@ export async function GET(req: Request) {
     }
 
     try {
-        console.log("tRYING ... CONNECT/ROUTE.TS")
+        // console.log("tRYING ... CONNECT/ROUTE.TS")
 
         const userClient = new TwitterApi({
             appKey: 'cHPUBUBZsQS4c9IcrELUQdohx',
             appSecret: '1DmuRzs9nKEhRKehWCeNSRtgduJfBPBAnbbkBX9KZN8iIrjU7C',
         })
 
-        console.log("App credentials declared")
+        // console.log("App credentials declared")
     
-        console.log("If this comes out, this is where it failed")
+        // console.log("If this comes out, this is where it failed")
         const {
             url, oauth_token, oauth_token_secret
         } = await userClient.generateAuthLink('https://ip.mediolano.app')
-        console.log("After failure point")
-        console.log(url)
-        console.log(oauth_token)
-        console.log(oauth_token_secret)
+        // console.log("After failure point")
+        // console.log(url)
+        // console.log(oauth_token)
+        // console.log(oauth_token_secret)
     
-        console.log("App credentials worked")
-        console.log('generate auth link worked')
+        // console.log("App credentials worked")
+        // console.log('generate auth link worked')
 
         const authData = {
             oauth_token, oauth_token_secret, timestamp: Date.now(), address
@@ -43,15 +43,15 @@ export async function GET(req: Request) {
         const storageObject = {
             tokenSecret: oauth_token_secret, address
         }
-        console.log("The code got here")
+        // console.log("The code got here")
         // await kv.set(`auth:${oauth_token}`, storageObject, { ex: 3600 })
         // console.log("Redis storage operation successful")
         const currentUser = await userClient.currentUser();
-        console.log(currentUser)
+        // console.log(currentUser)
 
         return NextResponse.json({ authUrl: url, data: currentUser })
     } catch (err) {
-        console.log("The catch block took this")
+        // console.log("The catch block took this")
         console.error("An error occured: ", err)
         return NextResponse.json(
             { error: "Failed to Instantiate X connection" },
