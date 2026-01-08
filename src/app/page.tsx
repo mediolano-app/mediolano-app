a9dcfd14a193492b86030fa43fe907835f4813ac
salvadorcamino <salvadorcamino@gmail.com>
2026-01-08 09:13:19 -0300

    start review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import StartHero from "@/components/discover/start-hero";
 import { HeroSlider } from "@/components/hero-slider"
-import { MediolanoFeatures } from "@/components/mediolano-features"
 
 export default function Home() {
   return (
     <div className="grid bg-background/30">
       <main className="flex flex-col row-start-2 p-6">
-        <MediolanoFeatures />
+        <HeroSlider />
       </main>
     </div>
   );
